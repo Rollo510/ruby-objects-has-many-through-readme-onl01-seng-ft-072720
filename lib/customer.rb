f50f05d6fb23918e27e3419cbf64dd3f5b9e36d9
Kevin Britt <kbritt510@gmail.com>
2020-08-14 18:20:09 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -26,7 +26,5 @@ class Customer
 alex = Customer.new("Alex", 30)
 rachel = Customer.new("Rachel", 27)
 dan = Waiter.new("Dan", 3)
-
-rachel.new_meal(dan, 50, 10)
-alex.new_meal(dan, 30, 5)
+binding.pry
 end
